820adfc327e905cd6531114a51afd4b1f2257705
Andrei-Cristian Sandor <andreicsandor@gmail.com>
2023-05-17 11:42:40 +0300

    Add basic UI interface & contacts list

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,14 +1,34 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { connectToServer, disconnectFromServer, sendMessageToServer } from "./Server";
+import {
+  connectToServer,
+  disconnectFromServer,
+  sendMessageToServer,
+} from "./Server";
+import api from "./Api";
 import Cookies from "js-cookie";
 import MessageDTO from "../dto/MessageDTO";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../static/styles.css";
+import {
+  Card,
+  CardBody,
+  CardLink,
+  CardSubtitle,
+  CardText,
+  CardTitle,
+  Col,
+  Row,
+} from "reactstrap";
+
 const ChatView = () => {
   const [user, setUser] = useState(null);
+  const [contacts, setContacts] = useState([]);
+  const [activeContact, setActiveContact] = useState(null);
   const [messages, setMessages] = useState([]);
   const [notifications, setNotifications] = useState([]);
-  
+
   const navigate = useNavigate();
 
   // Show error in console
@@ -25,7 +45,10 @@ const ChatView = () => {
   const onNotification = (response) => {
     console.log(response);
     var responseData = JSON.parse(response.body);
-    setNotifications((prevNotifications) => [...prevNotifications, responseData]);
+    setNotifications((prevNotifications) => [
+      ...prevNotifications,
+      responseData,
+    ]);
   };
 
   // Gets the logged-in user details from cookies
@@ -43,8 +66,20 @@ const ChatView = () => {
       // Disconenct from server
       disconnectFromServer();
     };
-
   }, [navigate]);
+
+  useEffect(() => {
+    const fetchContacts = async () => {
+      try {
+        const response = await api.get("/api/contacts");
+        setContacts(response.data);
+      } catch (error) {
+        console.error("An error occurred while fetching contacts:", error);
+      }
+    };
+
+    fetchContacts();
+  }, []);
 
   // Sets the default data input
   const [input, setInput] = useState({
@@ -61,12 +96,8 @@ const ChatView = () => {
   const handleSend = () => {
     // Prepare the message data transfer object
     if (input.recipient !== "" && input.content !== "") {
-      const message = new MessageDTO(
-        user,
-        input.recipient,
-        input.content
-      );
-      
+      const message = new MessageDTO(user, input.recipient, input.content);
+
       // Send the message to the server that sends it back to the corresponding user
       sendMessageToServer(message);
 
@@ -80,33 +111,84 @@ const ChatView = () => {
 
   return (
     <>
-    <div>
-      <h3>New message</h3>
-      <input
-        type="text"
-        name="recipient"
-        value={input.recipient}
-        onChange={handleInputChange}
-        placeholder="Type a recipient..."
-      />
-      <input
-        type="text"
-        name="content"
-        value={input.content}
-        onChange={handleInputChange}
-        placeholder="Type a message..."
-      />
-      <button onClick={handleSend}>Send</button>
-    </div>
-    <div>
-    {messages.map((message, index) => (
-      <div key={index}>
-        <p>Sender: {message.sender}</p>
-        <p>Message: {message.content}</p>
+      <div>
+        <h3>New message</h3>
+        <input
+          type="text"
+          name="recipient"
+          value={input.recipient}
+          onChange={handleInputChange}
+          placeholder="Type a recipient..."
+        />
+        <input
+          type="text"
+          name="content"
+          value={input.content}
+          onChange={handleInputChange}
+          placeholder="Type a message..."
+        />
+        <button onClick={handleSend}>Send</button>
       </div>
-    ))}
-  </div>
-  </>
+      <div>
+        {messages.map((message, index) => (
+          <div key={index}>
+            <p>Sender: {message.sender}</p>
+            <p>Message: {message.content}</p>
+          </div>
+        ))}
+      </div>
+
+      <Row className="m-5">
+  <Col sm="4">
+    <Card
+      body
+      className="card-custom"
+      style={{ height: "80vh" }}
+    >
+      <CardTitle tag="h5">Messenger</CardTitle>
+      <CardSubtitle className="mb-3 text-muted" tag="h6">
+        Contacts
+      </CardSubtitle>
+      <div
+        className="subcard-wrapper-custom"
+        style={{ overflowY: "auto", maxHeight: "80vh" }}
+      >
+        <Col sm="12">
+          {contacts.map((contact) => (
+            <Card body className="subcard-custom mx-1 my-2" onClick={() => setActiveContact(contact)}>
+              <CardTitle className="mb-1" tag="h6">
+                {contact.firstName} {contact.lastName}
+              </CardTitle>
+              <CardText className="small-text">
+                {contact.username}
+              </CardText>
+            </Card>
+          ))}
+        </Col>
+      </div>
+    </Card>
+  </Col>
+  <Col sm="8">
+    <Card
+      body
+      className="card-custom"
+      style={{ height: "80vh" }}
+    >
+      <CardTitle tag="h5">{activeContact ? `${activeContact.firstName} ${activeContact.lastName}` : 'Pick conversation'}</CardTitle>
+      <CardSubtitle className="mb-2 text-muted" tag="h6">
+        Online/Offline
+      </CardSubtitle>
+      <div
+        className="subcard-wrapper-custom"
+        style={{ overflowY: "auto", maxHeight: "80vh" }}
+      >
+        <Col sm="12"></Col>
+      </div>
+    </Card>
+  </Col>
+</Row>
+
+    </>
   );
 };
 
