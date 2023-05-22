9dd7a18202922982d7e33a5f5a38506f1443a5f5
Andrei-Cristian Sandor <andreicsandor@gmail.com>
2023-05-22 23:22:21 +0300

    Tidy up code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { LoginView, LogoutView } from "./components/Authentication";
-import ChatView from "./components/Chat"
+import ChatView from "./components/Chat";
 
 function App() {
   return (
     <Router>
       <div className="container-custom">
         <Routes>
+          <Route path="/" element={<Navigate to="/chat" replace />} />
           <Route path="/login" element={<LoginView />} />
           <Route path="/logout" element={<LogoutView />} />
           <Route path="/chat" element={<ChatView />} />
